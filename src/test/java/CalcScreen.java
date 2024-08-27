6b00c15310336e16f8a19fabb796290e5f89b992
Sadia Mahmuda <anika21.nextin@gmail.com>
2024-08-27 23:09:46 +0600

    Add Sub and clear method

--- src/test/java/CalcScreen.java
+++ src/test/java/CalcScreen.java
@@ -6,16 +6,20 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class CalcScreen {
-    @FindBy(id="com.google.android.calculator:id/digit_7")
-    WebElement btn7;
-    @FindBy(id="com.google.android.calculator:id/digit_5")
-    WebElement btn5;
+//    @FindBy(id="com.google.android.calculator:id/digit_7")
+//    WebElement btn7;
+//    @FindBy(id="com.google.android.calculator:id/digit_5")
+//    WebElement btn5;
     @FindBy(id="com.google.android.calculator:id/op_add")
     WebElement btnAdd;
+    @FindBy(id="com.google.android.calculator:id/op_sub")
+    WebElement btnSub;
     @FindBy(id="com.google.android.calculator:id/eq")
     WebElement btnEqual;
     @FindBy(id="com.google.android.calculator:id/result_final")
     WebElement txtResult;
+    @FindBy(id="com.google.android.calculator:id/clr")
+    WebElement btnClear;
 
     AndroidDriver driver;
 
@@ -37,5 +41,14 @@ public class CalcScreen {
 //        btn5.click();
 //        btnEqual.click();
 //        return txtResult.getText();
+    }
+
+    public int doSub(int num1, int num2) {
+
+        driver.findElement(By.id("com.google.android.calculator:id/digit_" + num1)).click();
+        btnSub.click();
+        driver.findElement(By.id("com.google.android.calculator:id/digit_" + num2)).click();
+        btnEqual.click();
+        return Integer.parseInt(txtResult.getText());
     }
 }
