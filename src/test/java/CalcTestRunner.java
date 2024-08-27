04976a17683f98a7abf18468ea4b15bd59258349
Sadia Mahmuda <anika21.nextin@gmail.com>
2024-08-27 22:43:53 +0600

    first commit

---
+++ src/test/java/CalcTestRunner.java
@@ -0,0 +1,13 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CalcTestRunner extends Setup{
+    @Test
+    public void doSum(){
+        CalcScreen calcScreen = new CalcScreen(driver);
+        String res = calcScreen.doSum();
+        System.out.println(res);
+        Assert.assertEquals(res, "12");
+    }
+
+}
